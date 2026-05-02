68ace7dff8446056cd2eb62ffea8dff9cdab8478
lu407540-ops <lu407540-ops@github.com>
2026-05-02 13:59:00 +0800

    fix: 添加缺失的 Canvas import

--- app/src/main/java/com/longscreenshot/ScreenshotService.java
+++ app/src/main/java/com/longscreenshot/ScreenshotService.java
@@ -6,6 +6,7 @@ import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.PixelFormat;
 import android.media.Image;
 import android.media.ImageReader;
